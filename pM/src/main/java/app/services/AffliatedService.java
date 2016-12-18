c9c96237dd5fa5a657b434b80d1c1fb8ea06f7b3
Bennett Qian <qian.cs@outlook.com>
2016-12-18 16:22:08 +0800

    Revert "Upload the code"
    This reverts commit 19564163a99f882b2c38a2885587d4882a455076.

--- pM/src/main/java/app/services/AffliatedService.java
+++
@@ -1,8 +0,0 @@
-package app.services;
-
-/**
- * Created by liuhaodong1 on 16/6/13.
- */
-public class AffliatedService {
-
-}
--- pM/src/main/java/com/example/pm/wxapi/WXEntryActivity.java
+++
@@ -1,8 +0,0 @@
-
-package com.example.pm.wxapi;
-
-import com.umeng.socialize.weixin.view.WXCallbackActivity;
-
-public class WXEntryActivity extends WXCallbackActivity {
-
-}
