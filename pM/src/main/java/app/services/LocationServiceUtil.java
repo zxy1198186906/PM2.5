ac2ca5635cfb85c741fc57bea8c3b1f82acc8447
洪嘉勇 <tjhjyisland@gmail.com>
2017-04-08 15:02:20 +0800

    change baidumap locationMode to Battery_Saving

--- pM/src/main/java/app/services/LocationServiceUtil.java
+++ pM/src/main/java/app/services/LocationServiceUtil.java
@@ -220,7 +220,7 @@ public class LocationServiceUtil implements LocationListener,GpsStatus.Listener
 
     private void baiduInitOption(){
         LocationClientOption option = new LocationClientOption();
-        option.setLocationMode(LocationMode.Hight_Accuracy);
+        option.setLocationMode(LocationMode.Battery_Saving);
         option.setCoorType("bd09ll");
         int span=1000;
         option.setScanSpan(span);
